580d10ea88ddf13f28adef08b5c2fcedf42cfd77
Li Peilin <lipeilin375@gmail.com>
2023-04-15 22:03:42 +0800

    Update limiter.ts

--- service/src/middleware/limiter.ts
+++ service/src/middleware/limiter.ts
@@ -5,7 +5,7 @@ const MAX_REQUEST_PER_HOUR = process.env.MAX_REQUEST_PER_HOUR
 
 const maxCount = (isNotEmptyString(MAX_REQUEST_PER_HOUR) && !isNaN(Number(MAX_REQUEST_PER_HOUR)))
   ? parseInt(MAX_REQUEST_PER_HOUR)
-  : 0 // 0 means unlimited
+  : 15 // 0 means unlimited
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // Maximum number of accesses within an hour
